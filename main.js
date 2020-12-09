68657da88ad7562f0b1b70d0f54ef7a5a6a74cb9
Zakariya Qureshi <zakariyaq313@gmail.com>
2020-12-09 23:57:27 +0530

    Some updates and fixes

--- main.js
+++ main.js
@@ -14,21 +14,27 @@
     document.querySelector("h2.month").textContent = `${day} ${month}`;
 }());
 
+
+// Main part from here
+
 let userInput = document.getElementById("search"),
     button = document.querySelectorAll("i.icon")[0],
     erase = document.querySelectorAll("i.icon")[1];
 
+// Prevent page reload on form submit 
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
 })
 
-function setWeather(data) {
-    let celsius = `${Math.round(parseFloat(data.main.temp) - 273.15)}°C`,
-        imageSource = document.querySelector("img.image"),
-        cardBackground = document.querySelector("div.card");
-    document.querySelector("p.weather").textContent = data.weather[0].main;
-    document.querySelector("h1.city").textContent = data.name;
-    document.querySelector("h2.temp").textContent = celsius;
+// Fill data in div card 
+function setWeather(data, count) {
+    let card = document.querySelectorAll("div.card")[count],
+        image = card.children[0].children[0],
+        celsius = `${Math.round(parseFloat(data.main.temp) - 273.15)}°C`;
+
+    card.children[0].children[1].textContent = data.weather[0].main;
+    card.children[1].children[1].textContent = data.name;
+    card.children[1].children[0].textContent = celsius;
 
     switch (data.weather[0].main) {
         case "Haze":
@@ -36,64 +42,75 @@ function setWeather(data) {
         case "Mist":
         case "Dust":
         case "Fog":
-            imageSource.src = "images/global-warming.svg";
-            cardBackground.style.backgroundImage = "radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,229,168,1) 0%, rgba(251,174,222,1) 100.7% )";
+            image.src = "images/global-warming.svg";
+            card.style.backgroundImage = "radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,229,168,1) 0%, rgba(251,174,222,1) 100.7% )";
             break;
 
         case "Clouds":
-            imageSource.src = "images/cloudy.svg";
-            cardBackground.style.backgroundImage = "linear-gradient( 114.2deg,  rgba(121,194,243,1) 22.6%, rgba(255,180,239,1) 67.7% )";
+            image.src = "images/cloudy.svg";
+            card.style.backgroundImage = "linear-gradient( 114.2deg,  rgba(121,194,243,1) 22.6%, rgba(255,180,239,1) 67.7% )";
             break;
 
         case "Snow":
-            imageSource.src = "images/snow.svg";
-            cardBackground.style.backgroundImage = "radial-gradient(circle 465px at -15.1% -25%, rgba(17,130,193,1) 0%, rgba(67,166,238,1) 49%, rgba(126,203,244,1) 90.2% )";
+            image.src = "images/snow.svg";
+            card.style.backgroundImage = "radial-gradient(circle 465px at -15.1% -25%, rgba(17,130,193,1) 0%, rgba(67,166,238,1) 49%, rgba(126,203,244,1) 90.2% )";
             break;
 
         case "Thunderstorm":
-            imageSource.src = "images/storm.svg";
-            cardBackground.style.backgroundImage = "linear-gradient( 109.6deg,  rgba(48,207,208,1) 11.2%, rgba(51,8,103,1) 92.5% )";
+            image.src = "images/storm.svg";
+            card.style.backgroundImage = "linear-gradient( 109.6deg,  rgba(48,207,208,1) 11.2%, rgba(51,8,103,1) 92.5% )";
             break;
 
         case "Clear":
-            imageSource.src = "images/sun.svg";
-            cardBackground.style.backgroundImage = "linear-gradient(to bottom right, #b3ffab, #12fff7)";
+            image.src = "images/sun.svg";
+            card.style.backgroundImage = "linear-gradient(to bottom right, #b3ffab, #12fff7)";
             break;
         
         case "Rain":
         case "Drizzle":
-            imageSource.src = "images/rainy.svg";
-            cardBackground.style.backgroundImage = "radial-gradient( circle 465px at -15.1% -25%,  rgba(17,130,193,1) 0%, rgba(67,166,238,1) 49%, rgba(126,203,244,1) 90.2% )";
+            image.src = "images/rainy.svg";
+            card.style.backgroundImage = "radial-gradient( circle 465px at -15.1% -25%,  rgba(17,130,193,1) 0%, rgba(67,166,238,1) 49%, rgba(126,203,244,1) 90.2% )";
             break;
         
         default:
-            imageSource = "images/wind.svg";
-            cardBackground.style.backgroundImage = "radial-gradient( circle farthest-corner at 10% 20%,  rgba(253,101,133,1) 0%, rgba(255,211,165,1) 90% )";
+            image.src = "images/wind.svg";
+            card.style.backgroundImage = "radial-gradient( circle farthest-corner at 10% 20%,  rgba(253,101,133,1) 0%, rgba(255,211,165,1) 90% )";
             break;
     }
 }
 
-function getWeather() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&appid=b0c90ee33b0ac413a9614d297708aa07`) 
+// Get weather from API 
+function getWeather(count, cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName || userInput.value}&appid=b0c90ee33b0ac413a9614d297708aa07`) 
     .then( (response) => { 
         return response.json();
     })
     
     .then( (data) => {
-      setWeather(data);
+      setWeather(data, count);
     })
 
     .catch( () => {
       // catch any errors
+      console.log("Fuck that");
     });
 }
 
-let cards = document.querySelectorAll("div.card");
-function removeCard() {
+let defaultCities = () => {
+    getWeather(0, "Delhi");
+    getWeather(1, "Pune");
+    getWeather(2, "Bangalore");
+    getWeather(3, "Mumbai");
+}
+
+defaultCities();
+
+function removeCards() {
+    let cards = document.querySelectorAll("div.card");    
     for (let i = 0; i < cards.length; i++) {
         cards[i].remove();
     }
-}
+} // In order to remove default divs
 
 function addCard() {
     let newCard = document.createElement("div");
@@ -107,19 +124,23 @@ function addCard() {
                                 <h2 class="temp"></h2>
                                 <h1 class="city"></h1>
                             </span>`;
-}
+    document.querySelector("div.cards").style.gridTemplateColumns = "1fr";
+    getWeather(0, userInput.value);
+} //In order to add new div
 
 button.addEventListener("click", () => {
-    getWeather();
-    removeCard();
+    removeCards();
     addCard();
 })
 
 userInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-        getWeather();
-        removeCard();
-        addCard();
+        if (userInput.value.trim() === "") {
+            console.log("This fucker right here");
+        } else {
+            removeCards();
+            addCard();   
+        }
     }
 })
 
